15d0afb0efc8556585400c4b01740776a9ef302b
Marcin Drozd <marcindrozd@me.com>
2015-04-09 10:50:01 +0200

    add relationships between player and match

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,6 @@
 class Player < ActiveRecord::Base
+  has_many :host_matches, class_name: 'Match', foreign_key: 'player1_id'
+  has_many :guest_matches, class_name: 'Match', foreign_key: 'player2_id'
+
   validates_presence_of :first_name, :last_name
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe Player do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
+  it { should have_many(:host_matches).class_name('Match').with_foreign_key(:player1_id) }
+  it { should have_many(:guest_matches).class_name('Match').with_foreign_key(:player2_id) }
 end
