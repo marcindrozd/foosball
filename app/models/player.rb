c9fe0ad8e8ce07d5087f6e2992f5481562937b77
Marcin Drozd <marcindrozd@me.com>
2015-04-09 11:24:25 +0200

    add devise for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :host_matches, class_name: 'Match', foreign_key: 'player1_id'
   has_many :guest_matches, class_name: 'Match', foreign_key: 'player2_id'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :players
   resources :matches, only: [:new, :create]
 end
