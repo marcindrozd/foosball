eecbd62c3e78b176452750b6d4d05579d2071c28
Marcin Drozd <marcindrozd@me.com>
2015-04-09 15:46:39 +0200

    add carrierwave

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml'
 gem 'devise'
 gem 'figaro'
+gem 'carrierwave'
 
 group :development, :test do
   gem 'byebug'
--- app/models/player.rb
+++ app/models/player.rb
@@ -6,5 +6,7 @@ class Player < ActiveRecord::Base
   has_many :host_matches, class_name: 'Match', foreign_key: 'player1_id'
   has_many :guest_matches, class_name: 'Match', foreign_key: 'player2_id'
 
+  mount_uploader :avatar, AvatarUploader
+
   validates_presence_of :first_name, :last_name
 end
