414cbe673d1cd1c362e704dc865cd42a4bfe559f
Marcin Drozd <marcindrozd@me.com>
2015-04-09 17:57:04 +0200

    update create action for match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,5 +4,12 @@ class MatchesController < ApplicationController
   end
 
   def create
+    @match = Match.new(match_params)
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:player1, :player2, :score_player1, :score_player2)
   end
 end
