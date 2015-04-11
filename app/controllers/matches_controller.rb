75b53919f2b52ae86a22e99d4ffa3ec2738bf994
Marcin Drozd <marcindrozd@me.com>
2015-04-11 12:32:07 +0200

    update to add match form

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -16,6 +16,6 @@ class MatchesController < ApplicationController
   private
 
   def match_params
-    params.require(:match).permit(:player1, :player2, :score_player1, :score_player2)
+    params.require(:match).permit(:player1_id, :player2_id, :score_player1, :score_player2)
   end
 end
