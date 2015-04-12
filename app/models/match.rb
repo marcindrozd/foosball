c4437495f7e15061e4dd6e56a2af32f52097fd12
Marcin Drozd <marcindrozd@me.com>
2015-04-12 12:32:59 +0200

    add validations for score

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,5 +2,6 @@ class Match < ActiveRecord::Base
   belongs_to :player1, class_name: 'Player'
   belongs_to :player2, class_name: 'Player'
 
-  validates_presence_of :player1_id, :player2_id, :score_player1, :score_player2
+  validates_presence_of :player1_id, :player2_id, :score_player1, :score_player2, :match_date
+  validates_numericality_of :score_player1, :score_player2, { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -5,6 +5,15 @@ describe Match do
   it { should validate_presence_of :player2_id }
   it { should validate_presence_of :score_player1 }
   it { should validate_presence_of :score_player2 }
+  it { should validate_presence_of :match_date }
   it { should belong_to(:player1).class_name('Player') }
   it { should belong_to(:player2).class_name('Player') }
+  it { should validate_numericality_of(:score_player1).only_integer }
+  it { should validate_numericality_of(:score_player2).only_integer }
+  it { should_not allow_value(-1).for(:score_player1) }
+  it { should_not allow_value(-1).for(:score_player2) }
+  it { should allow_value(0).for(:score_player1) }
+  it { should allow_value(0).for(:score_player2) }
+  it { should_not allow_value(11).for(:score_player1) }
+  it { should_not allow_value(11).for(:score_player1) }
 end
