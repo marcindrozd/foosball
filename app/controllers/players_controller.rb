027d0776c7403e6ca0dc443e5eadd7511fb94ee8
Marcin Drozd <marcindrozd@me.com>
2015-04-12 14:18:29 +0200

    add sorting to the rankings page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,21 @@
 class PlayersController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def show
     @player = Player.find(params[:id])
   end
 
   def index
-    @players = Player.all
+    @players = Player.order(sort_column + ' ' + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    Player.column_names.include?(params[:sort]) ? params[:sort] : "points"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   def pretty_date(date)
     date.strftime("%d %b %Y")
   end
+
+  def sortable(column, title = nil)
+    title ||= column.split("_").join(" ").titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_column && sort_direction == 'desc') ? 'asc' : 'desc'
+    link_to title, { sort: column, direction: direction }, { class: css_class }
+  end
 end
