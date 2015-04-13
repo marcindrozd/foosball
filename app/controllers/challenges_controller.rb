b9221843054049b78ae49937c50449e569aa26aa
Marcin Drozd <marcindrozd@me.com>
2015-04-13 09:56:54 +0200

    fix mailer tests

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -5,7 +5,7 @@ class ChallengesController < ApplicationController
     @recipient = Player.find(params[:player_id])
     @sender = current_player
     @message = params[:message]
-    PlayerMailer.send_challenge(@sender, @recipient, @message).deliver
+    PlayerMailer.send_challenge(@sender, @recipient, @message).deliver_now
     flash[:notice] = "Challenge has been sent!"
     redirect_to @recipient
   end
--- spec/mailers/player_mailer_spec.rb
+++ spec/mailers/player_mailer_spec.rb
@@ -2,16 +2,18 @@ require "spec_helper"
 
 describe PlayerMailer do
   describe "send_challenge" do
-    let(:mail) { PlayerMailer.send_challenge }
+    let(:alice) { create(:player, email: "alice@example.com") }
+    let(:bob) { create(:player, email: "bob@example.com") }
+    let(:mail) { PlayerMailer.send_challenge(bob, alice, "Let's play!") }
 
     it "renders the headers" do
-      mail.subject.should eq("Send challenge")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("You have been challenged!")
+      mail.to.should eq(["alice@example.com"])
+      mail.from.should eq(["bob@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("Let's play!")
     end
   end
 
